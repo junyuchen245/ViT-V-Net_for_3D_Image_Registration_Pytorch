19559170e915b44e181c83ffe9571533340e9320
Junyu Chen <jchen245@jhmi.edu>
2021-08-06 09:56:34 -0400

    Added mutual information and local mutual information loss

--- ViT-V-Net/losses.py
+++ ViT-V-Net/losses.py
@@ -341,3 +341,101 @@ class MutualInformation(torch.nn.Module):
 
     def forward(self, y_true, y_pred):
         return -self.mi(y_true, y_pred)
+    
+class localMutualInformation(torch.nn.Module):
+    """
+    Local Mutual Information for non-overlapping patches
+    """
+    def __init__(self, sigma_ratio=1, minval=0., maxval=1., num_bin=32, patch_size=5):
+        super(localMutualInformation3D, self).__init__()
+
+        """Create bin centers"""
+        bin_centers = np.linspace(minval, maxval, num=num_bin)
+        vol_bin_centers = Variable(torch.linspace(minval, maxval, num_bin), requires_grad=False).cuda()
+        num_bins = len(bin_centers)
+
+        """Sigma for Gaussian approx."""
+        sigma = np.mean(np.diff(bin_centers)) * sigma_ratio
+
+        self.preterm = 1 / (2 * sigma**2)
+        self.bin_centers = bin_centers
+        self.max_clip = maxval
+        self.num_bins = num_bins
+        self.vol_bin_centers = vol_bin_centers
+        self.patch_size = patch_size
+
+    def local_mi(self, y_true, y_pred):
+        y_pred = torch.clamp(y_pred, 0., self.max_clip)
+        y_true = torch.clamp(y_true, 0, self.max_clip)
+        
+        """Reshape bin centers"""
+        o = [1, 1, np.prod(self.vol_bin_centers.shape)]
+        vbc = torch.reshape(self.vol_bin_centers, o).cuda()
+        
+        """Making image paddings"""
+        if len(list(y_pred.size())[2:]) == 3:
+            ndim = 3
+            x, y, z = list(y_pred.size())[2:]
+            # compute padding sizes
+            x_r = -x % self.patch_size
+            y_r = -y % self.patch_size
+            z_r = -z % self.patch_size
+            padding = (z_r // 2, z_r - z_r // 2, y_r // 2, y_r - y_r // 2, x_r // 2, x_r - x_r // 2, 0, 0, 0, 0)
+        elif len(list(y_pred.size())[2:]) == 2:
+            ndim = 2
+            x, y = list(y_pred.size())[2:]
+            # compute padding sizes
+            x_r = -x % self.patch_size
+            y_r = -y % self.patch_size
+            padding = (y_r // 2, y_r - y_r // 2, x_r // 2, x_r - x_r // 2, 0, 0, 0, 0)
+        else:
+            raise Exception('Supports 2D and 3D but not {}'.format(list(y_pred.size())))
+        y_true = F.pad(y_true, padding, "constant", 0)
+        y_pred = F.pad(y_pred, padding, "constant", 0)
+        
+        """Reshaping images into non-overlapping patches"""
+        if ndim == 3:
+            y_true_patch = torch.reshape(y_true, (y_true.shape[0], y_true.shape[1],
+                                            (x + x_r) // self.patch_size, self.patch_size,
+                                            (y + y_r) // self.patch_size, self.patch_size,
+                                            (z + z_r) // self.patch_size, self.patch_size))
+            y_true_patch = y_true_patch.permute(0, 1, 2, 4, 6, 3, 5, 7)
+            y_true_patch = torch.reshape(y_true_patch, (-1, self.patch_size ** 3, 1))
+
+            y_pred_patch = torch.reshape(y_pred, (y_pred.shape[0], y_pred.shape[1],
+                                            (x + x_r) // self.patch_size, self.patch_size,
+                                            (y + y_r) // self.patch_size, self.patch_size,
+                                            (z + z_r) // self.patch_size, self.patch_size))
+            y_pred_patch = y_pred_patch.permute(0, 1, 2, 4, 6, 3, 5, 7)
+            y_pred_patch = torch.reshape(y_pred_patch, (-1, self.patch_size ** 3, 1))
+        else:
+            y_true_patch = torch.reshape(y_true, (y_true.shape[0], y_true.shape[1],
+                                            (x + x_r) // self.patch_size, self.patch_size,
+                                            (y + y_r) // self.patch_size, self.patch_size))
+            y_true_patch = y_true_patch.permute(0, 1, 2, 4, 3, 5, 6)
+            y_true_patch = torch.reshape(y_true_patch, (-1, self.patch_size ** 2, 1))
+
+            y_pred_patch = torch.reshape(y_pred, (y_pred.shape[0], y_pred.shape[1],
+                                            (x + x_r) // self.patch_size, self.patch_size,
+                                            (y + y_r) // self.patch_size, self.patch_size))
+            y_pred_patch = y_pred_patch.permute(0, 1, 2, 4, 3, 5, 6)
+            y_pred_patch = torch.reshape(y_pred_patch, (-1, self.patch_size ** 2, 1))
+        
+        """Compute MI"""
+        I_a_patch = torch.exp(- self.preterm * torch.square(y_true_patch - vbc))
+        I_a_patch = I_a_patch / torch.sum(I_a_patch, dim=-1, keepdim=True)
+
+        I_b_patch = torch.exp(- self.preterm * torch.square(y_pred_patch - vbc))
+        I_b_patch = I_b_patch / torch.sum(I_b_patch, dim=-1, keepdim=True)
+        
+        pab = torch.bmm(I_a_patch.permute(0, 2, 1), I_b_patch)
+        pab = pab / self.patch_size ** ndim
+        pa = torch.mean(I_a_patch, dim=1, keepdim=True)
+        pb = torch.mean(I_b_patch, dim=1, keepdim=True)
+
+        papb = torch.bmm(pa.permute(0, 2, 1), pb) + 1e-6
+        mi = torch.sum(torch.sum(pab * torch.log(pab / papb + 1e-6), dim=1), dim=1)
+        return mi.mean()
+
+    def forward(self,y_true, y_pred):
+        return -self.local_mi(y_true, y_pred)
